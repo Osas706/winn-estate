d163229c36ecbd6555c4962ef5016b7fc6afdab1
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-03-06 12:13:39 +0100

    create and add Oauth component

--- api/modals/user.modal.js
+++ api/modals/user.modal.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: 'https://st2.depositphotos.com/1009634/7235/v/450/depositphotos_72350117-stock-illustration-no-user-profile-picture-hand.jpg',
+    },
 },{timestamps: true});
 
 const User = mongoose.model('User', userSchema);
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBj5zJmjRoGyPyrgVKqtVTJKuS6vMpNQI0",
+  authDomain: "winn-estate.firebaseapp.com",
+  projectId: "winn-estate",
+  storageBucket: "winn-estate.appspot.com",
+  messagingSenderId: "21770465851",
+  appId: "1:21770465851:web:79fbbc30cda00d4f1f621e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
