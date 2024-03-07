f582810c3c9a530ee51d00bcfb6fbbb2c7f720ad
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-03-07 11:37:18 +0100

    add del user func

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRef } from "react";
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart, updateUserSuccess, updateUserFailure } from "../redux/user/userSlice";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  deleteUserFailure
+} from "../redux/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef(null);
@@ -24,7 +36,7 @@ const Profile = () => {
   //request.resource.contentType.matches('image/.*')
 
   useEffect(() => {
-    if(file){
+    if (file) {
       handleFileUpload(file);
     }
   }, [file]);
@@ -35,23 +47,23 @@ const Profile = () => {
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed', 
+    uploadTask.on(
+      "state_changed",
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFileP(Math.round(progress));
       },
 
       (error) => {
         setFileUploadError(true);
       },
-  
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref)
-          .then((downloadURL) => {
-            setFormData({ ...formData, avatar: downloadURL });
-          })
-      },
 
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
     );
   };
 
@@ -66,27 +78,50 @@ const Profile = () => {
       dispatch(updateUserStart());
 
       const res = await fetch(`/api/user/update/${currentUser._id}`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
 
       const data = await res.json();
 
-      if(data.sucsess === false){
+      if (data.sucsess === false) {
         dispatch(updateUserFailure(data.message));
         return;
-      };
+      }
 
       dispatch(updateUserSuccess(data));
       setUpdateSuccess(true);
     } catch (error) {
       dispatch(updateUserFailure(error.message));
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+
+      const data = await res.json();
+
+      if(data.sucsess === false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      };
+
+      dispatch(deleteUserSuccess(data));
+
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
     };
   };
 
+  //template....................
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -107,17 +142,17 @@ const Profile = () => {
           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
         />
 
-        <p className='text-sm self-center'>
+        <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className='text-red-700'>
+            <span className="text-red-700">
               Error Image upload (image must be less than 2 mb)
             </span>
           ) : fileP > 0 && fileP < 100 ? (
-            <span className='text-slate-700'>{`Uploading ${fileP}%`}</span>
+            <span className="text-slate-700">{`Uploading ${fileP}%`}</span>
           ) : fileP === 100 ? (
-            <span className='text-green-700'>Image successfully uploaded!</span>
+            <span className="text-green-700">Image successfully uploaded!</span>
           ) : (
-            ''
+            ""
           )}
         </p>
 
@@ -147,19 +182,25 @@ const Profile = () => {
           onChange={handleChange}
         />
 
-        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          {loading ? 'Loading...' : 'Update'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
 
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
+          Delete Account
+        </span>
+
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
 
-      <p className="text-red-700 mt-5">{error ? error : ''}</p>
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
-        {updateSuccess ? 'User is updated successfully' : ''}
+        {updateSuccess ? "User is updated successfully" : ""}
       </p>
     </div>
   );
