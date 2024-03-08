7bb508f3111070f268934f7e5becb95ecde953df
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-03-08 19:09:24 +0100

    .

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -84,6 +84,7 @@ const CreateListing = () => {
     })
   };
 
+  //*************template**************** */
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
