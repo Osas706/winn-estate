193f91a1133165bffc9159efef3b861f252394b7
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-05-06 10:00:40 +0100

    correction

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -20,7 +20,7 @@ export const updateUser = async ( req, res, next) => {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
             $set: {
                 username: req.body.username,
-                email: req.body.password,
+                email: req.body.email,
                 password: req.body.password,
                 avatar: req.body.avatar,
             }
